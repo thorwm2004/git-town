a30c8bd20a36c21e33d29875a0ba82215cf80f81
Kevin Goslar <kevin.goslar@gmail.com>
2015-09-29 07:04:00 -0700

    temp

--- features/git-sync/current_branch/feature_branch/no_conflict/deleted_tracking_branch.feature
+++ features/git-sync/current_branch/feature_branch/no_conflict/deleted_tracking_branch.feature
@@ -1,8 +1,8 @@
 Feature: git sync: restores deleted tracking branch
 
   As a developer syncing a feature branch whose tracking branch has been deleted
-  I want a new tracking branch to be created
-  So that my work is safe in case my local copy gets lost.
+  I want to be asked whether to delete that branch from my local machine
+  So that I can keep my local client in sync with what is happening on the remote repository.
 
 
   Background:
@@ -12,19 +12,17 @@ Feature: git sync: restores deleted tracking branch
       | feature | local and remote | feature commit | feature_file |
     And the "feature" branch gets deleted on the remote
     And I am on the "feature" branch
-    When I run `git sync`
+    When I run `git sync` and enter "yes"
 
 
+  @debug-commands
   Scenario: result
-    Then it runs the commands
+    Then I see "The branch 'feature' was deleted on the remote"
+    And it runs the commands
       | BRANCH  | COMMAND                    |
       | feature | git fetch --prune          |
       |         | git checkout main          |
       | main    | git rebase origin/main     |
-      |         | git checkout feature       |
-      | feature | git merge --no-edit main   |
-      |         | git push -u origin feature |
-    And I am still on the "feature" branch
-    And I have the following commits
-      | BRANCH  | LOCATION         | MESSAGE        | FILE NAME    |
-      | feature | local and remote | feature commit | feature_file |
+      |         | git branch -D feature      |
+    And I end up on the "main" branch
+    And there are no commits
--- src/helpers/sync_helpers.sh
+++ src/helpers/sync_helpers.sh
@@ -6,7 +6,7 @@
 # Outputs the steps to sync the given branch
 function sync_branch_steps {
   local branch=$1
-  local is_feature ; is_feature="$(is_feature_branch "$branch")"
+  local is_feature="$(is_feature_branch "$branch")"
 
   # If there is a remote origin, then checkout and sync all branches because
   # there may be changes to perennial branches, otherwise only sync feature
@@ -25,7 +25,29 @@ function sync_branch_steps {
       if [ "$(has_tracking_branch "$branch")" == true ]; then
         step "push_branch $branch"
       else
-        step "create_tracking_branch $branch"
+        # tracking branch got deleted on the remote
+        echo "The branch '$branch' was deleted on the remote."
+        echo "This most likely means it was shipped or deleted by another developer."
+        echo "Do you want to remove it from your local copy?"
+        echo "If you answer 'n', this branch will be kept and pushed back to the repository."
+        echo -n "(y/n)"
+        read user_input
+        read user_input_2
+        echo "XXXXXXXXXXXXX"
+        echo "$user_input"
+        echo "YYYYYYYYYYYYY"
+        echo "$user_input_2"
+        if [ "$user_input" == "y" ]; then
+          # User wants to delete the current branch from the local machine
+          step "delete_local_branch $branch force"
+          local parent_branch_name=$(parent_branch "$branch")
+          steps_to_update_child_branches "$branch" "$parent_branch_name"
+          step "delete_parent_entry $branch"
+          step "delete_all_ancestor_entries"
+        else
+          # User wants to keep the current branch --> push it to the remote
+          step "create_tracking_branch $branch"
+        fi
       fi
     fi
   fi
